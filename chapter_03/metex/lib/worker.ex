bb4608c584da7db5055a85aad5598302bad05473
Meraj <meraj.enigma@gmail.com>
2023-10-23 12:16:26 -0400

    metex initial version

---
+++ chapter_03/metex/lib/worker.ex
@@ -0,0 +1,37 @@
+defmodule Worker do
+  @moduledoc false
+
+  def temperature_of(location) do
+    result = url_for(location) |> Req.get!() |> parse_response()
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  def url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{api_key()}"
+  end
+
+  defp parse_response(%Req.Response{status: 200, body: body}), do: compute_temperature(body)
+
+  defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp api_key do
+    Application.fetch_env!(:metex, :api_key)
+  end
+end
