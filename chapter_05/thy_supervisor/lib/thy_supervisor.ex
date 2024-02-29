42aad880e1494f53d411ccb128e82d66039cec29
Meraj Molla <meraj.enigma@gmail.com>
2024-02-29 11:05:09 -0500

    restart_child, count_children, which_children, terminate and handle crash

--- chapter_05/thy_supervisor/lib/thy_supervisor.ex
+++ chapter_05/thy_supervisor/lib/thy_supervisor.ex
@@ -12,8 +12,24 @@ defmodule ThySupervisor do
     GenServer.call(supervisor, {:start_child, child_spec})
   end
 
+  def restart_child(supervisor, pid, child_spec) when is_pid(pid) do
+    GenServer.call(supervisor, {:restart_child, pid, child_spec})
+  end
+
+  def count_children(supervisor) do
+    GenServer.call(supervisor, :count_children)
+  end
+
+  def which_children(supervisor) do
+    GenServer.call(supervisor, :which_children)
+  end
+
   def terminate_child(supervisor, pid) when is_pid(pid) do
     GenServer.call(supervisor, {:terminate_child, pid})
+  end
+
+  def terminate(supervisor) do
+    GenServer.call(supervisor, :terminate)
   end
 
   # callbacks
@@ -42,11 +58,39 @@ defmodule ThySupervisor do
   end
 
   @impl GenServer
+  def handle_call({:restart_child, old_pid, child_spec}, _from, state) do
+    case Map.fetch(state, old_pid) do
+      {:ok, ^child_spec} ->
+        case restart_child(old_pid, child_spec) do
+          {:ok, {new_pid, child_spec}} ->
+            new_state =
+              state
+              |> Map.delete(old_pid)
+              |> Map.put(new_pid, child_spec)
+
+            {:reply, {:ok, new_pid}, new_state}
+
+          :error ->
+            {:reply, {:error, "error restarting child"}, state}
+        end
+
+      _ ->
+        {:reply, :ok, state}
+    end
+  end
+
+  @impl GenServer
+  def handle_call(:terminate, _from, state) do
+    terminate_children(state)
+    {:reply, :ok, state}
+  end
+
+  @impl GenServer
   def handle_call({:terminate_child, pid}, _from, state) do
     case terminate_child(pid) do
       :ok ->
         new_state = Map.delete(state, pid)
-        {:reply, {:ok, pid}, new_state}
+        {:reply, :ok, new_state}
 
       :error ->
         {:reply, {:error, "error terminating child"}, state}
@@ -54,9 +98,40 @@ defmodule ThySupervisor do
   end
 
   @impl GenServer
-  def handle_call({:EXIT, from, :killed}, state) do
+  def handle_call(:count_children, _from, state) do
+    {:reply, map_size(state), state}
+  end
+
+  @impl GenServer
+  def handle_call(:which_children, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl GenServer
+  def handle_info({:EXIT, from, :killed}, state) do
     new_state = Map.delete(state, from)
-    {:noreply, state}
+    {:noreply, new_state}
+  end
+
+  def handle_info({:EXIT, old_pid, _reason}, state) do
+    case Map.fetch(state, old_pid) do
+      {:ok, child_spec} ->
+        case restart_child(old_pid, child_spec) do
+          {:ok, {new_pid, child_spec}} ->
+            new_state =
+              state
+              |> Map.delete(old_pid)
+              |> Map.put(new_pid, child_spec)
+
+            {:noreply, new_state}
+
+          :error ->
+            {:noreply, state}
+        end
+
+      _ ->
+        {:noreply, state}
+    end
   end
 
   # Helper functions
@@ -81,6 +156,28 @@ defmodule ThySupervisor do
       _ ->
         :error
     end
+  end
+
+  defp restart_child(pid, child_spec) when is_pid(pid) do
+    case terminate_child(pid) do
+      :ok ->
+        case start_child(child_spec) do
+          {:ok, new_pid} ->
+            {:ok, {new_pid, child_spec}}
+
+          :error ->
+            :error
+        end
+
+      :error ->
+        :error
+    end
+  end
+
+  defp terminate_children([]), do: :ok
+
+  defp terminate_children(state) do
+    Enum.map(state, fn {pid, _child_spec} -> terminate_child(pid) end)
   end
 
   defp terminate_child(pid) do
