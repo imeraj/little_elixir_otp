deef22110cacb270ac6c9d5c596c9e60492ddb10
Meraj Molla <meraj.enigma@gmail.com>
2024-03-04 11:26:40 -0500

    init commit to start and restart children using dynamic supervisor

---
+++ chapter_06/pooly/lib/pooly/worker.ex
@@ -0,0 +1,23 @@
+defmodule Worker do
+  @moduledoc false
+
+  use GenServer
+
+  # API
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, :ok, [])
+  end
+
+  def stop(pid) when is_pid(pid) do
+    GenServer.call(pid, :stop)
+  end
+
+  # callbacks
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  def handle_call(:stop, _from, state) do
+    {:stop, :normal, :ok, state}
+  end
+end
---
+++ chapter_06/pooly/lib/pooly/worker_supervisor.ex
@@ -0,0 +1,22 @@
+defmodule Pooly.WorkerSupervisor do
+  @moduledoc false
+
+  use DynamicSupervisor
+
+  # API
+  def start_link do
+    DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def start_child({m, f, a}) do
+    spec = %{id: Worker, start: {m, f, a}}
+    DynamicSupervisor.start_child(__MODULE__, spec)
+  end
+
+  # callbacks
+  def init(init_arg) do
+    opts = [strategy: :one_for_one, max_restarts: 5, max_seconds: 5, extra_arguments: [init_arg]]
+
+    DynamicSupervisor.init(opts)
+  end
+end
